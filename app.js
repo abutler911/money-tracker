5e233d53ac3ee8a2a57e2156afcda5107d733c5d
Andrew Butler <abutler911@gmail.com>
2024-03-07 18:47:50 -0700

    added layouts

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const savingsRouter = require("./routes/savings");
 const savingsController = require("./controllers/savingsController");
-const debtRouter = require("./routes/debt");
 const debtController = require("./controllers/debtController");
 const Savings = require("./models/savings");
 const Debt = require("./models/debt");
+const expressLayouts = require("express-ejs-layouts");
 
 // Load environment variables from .env file
 dotenv.config();
@@ -22,6 +21,8 @@ app.use(bodyParser.json());
 
 // Set up EJS as the view engine
 app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
 
 // Connect to MongoDB using the connection string from the .env file
 const mongoURI = process.env.MONGODB_URI;
@@ -30,20 +31,41 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-// Route for the admin section
-app.get("/admin", (req, res) => {
-  res.render("admin");
+// Route for the password input screen
+app.get("/", (req, res) => {
+  res.render("index", { title: "Money Tracker" });
 });
 
-// Define routes
-app.get("/", async (req, res) => {
+// Middleware to handle password submission
+app.post("/login", (req, res, next) => {
+  const password = req.body.password;
+  if (password === process.env.APP_PASSWORD) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+});
+
+// Middleware for error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// Middleware for the main page
+app.get("/dashboard", async (req, res) => {
   try {
     const savings = await Savings.find();
     const debt = await Debt.find();
     res.render("index", { savings, debt });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    next(err);
   }
+});
+
+// Routes for the admin section
+app.get("/admin", (req, res) => {
+  res.render("admin");
 });
 
 // Routes for savings

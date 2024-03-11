a13efe90c91871a43901667e697add42319e95c5
abutler911 <abutler911@gmail.com>
2024-03-11 10:56:34 -0600

    tidying up login functionality

---
+++ public/javascript/script.js
@@ -0,0 +1,8 @@
+function trimInputs() {
+  var usernameInput = document.getElementById("username");
+  var passwordInput = document.getElementById("password");
+
+  // Trim leading and trailing whitespace from the input values
+  usernameInput.value = usernameInput.value.trim();
+  passwordInput.value = passwordInput.value.trim();
+}

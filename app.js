2611c50ff036bbf3cf4594bcac6342f864c01d4d
abutler911 <abutler911@gmail.com>
2024-03-12 13:35:32 -0600

    update functionality

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const dotenv = require("dotenv");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const savingsController = require("./controllers/savingsController");
-const debtController = require("./controllers/debtController");
 const Savings = require("./models/savings");
 const Debt = require("./models/debt");
 const expressLayouts = require("express-ejs-layouts");
@@ -20,7 +18,7 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, "public")));
@@ -93,6 +91,17 @@ app.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
+// Logout route
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      console.error("Error logging out:", err);
+      res.status(500).send("Error logging out");
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
 app.get("/register", (req, res) => {
   res.render("register", { title: "Register" });
 });
@@ -109,29 +118,12 @@ app.post("/register", async (req, res) => {
     res.status(500).send("Failed to register user");
   }
 });
-const getTotalSavingsAmount = (savings) => {
-  let totalAmount = 0;
-  savings.forEach((saving) => {
-    totalAmount += saving.amount;
-  });
-  return totalAmount;
-};
-
-const getTotalDebtAmount = (debt) => {
-  let totalAmount = 0;
-  debt.forEach((debt) => {
-    totalAmount += debt.amount;
-  });
-  return totalAmount;
-};
 
 app.get("/dashboard", isAuthenticated, async (req, res, next) => {
   try {
     const user = req.user;
     const savings = await Savings.find();
     const debt = await Debt.find();
-    const totalSavingsAmount = getTotalSavingsAmount(savings);
-    const totalDebtAmount = getTotalDebtAmount(debt);
     res.render("dashboard", {
       title: "Penny Pal Dashboard",
       user,
@@ -164,28 +156,17 @@ app.get("/add-savings", isAuthenticated, (req, res) => {
 
 app.post("/add-savings", isAuthenticated, async (req, res) => {
   try {
-    // Ensure user is admin
     if (!req.user.isAdmin) {
       return res
         .status(403)
         .send("You do not have permission to perform this action.");
     }
-
-    // Extract account name and amount from request body
     const { accountName, amount } = req.body;
-
-    // Validate input (e.g., ensure amount is a valid number)
-
-    // Create new savings object
     const savings = new Savings({
       accountName,
       amount,
     });
-
-    // Save the savings object to the database
     await savings.save();
-
-    // Redirect to dashboard with success message
     res.redirect("/dashboard");
   } catch (error) {
     console.error("Error adding savings:", error);
@@ -195,20 +176,13 @@ app.post("/add-savings", isAuthenticated, async (req, res) => {
 
 app.post("/delete-saving/:id", isAuthenticated, async (req, res) => {
   try {
-    // Ensure user is admin
     if (!req.user.isAdmin) {
       return res
         .status(403)
         .send("You do not have permission to perform this action.");
     }
-
-    // Extract the savings account ID from the request parameters
     const { id } = req.params;
-
-    // Find the savings account by ID and delete it
     await Savings.findByIdAndDelete(id);
-
-    // Redirect to the dashboard with a success message
     res.redirect("/dashboard");
   } catch (error) {
     console.error("Error deleting savings:", error);
@@ -232,26 +206,17 @@ app.get("/add-debt", isAuthenticated, (req, res) => {
 app.post("/add-debt", isAuthenticated, async (req, res) => {
   console.log(req.body);
   try {
-    // Ensure user is admin
     if (!req.user.isAdmin) {
       return res
         .status(403)
         .send("You do not have permission to perform this action.");
     }
-
-    // Extract debt information from request body
     const { accountName, amount } = req.body;
-
-    // Create a new debt object
     const debt = new Debt({
       accountName,
       amount,
     });
-
-    // Save the debt object to the database
     await debt.save();
-
-    // Redirect to the dashboard with a success message
     res.redirect("/dashboard");
   } catch (error) {
     console.error("Error adding debt:", error);
@@ -261,21 +226,14 @@ app.post("/add-debt", isAuthenticated, async (req, res) => {
 
 app.post("/update-debt/:id", isAuthenticated, async (req, res) => {
   try {
-    // Ensure user is admin
     if (!req.user.isAdmin) {
       return res
         .status(403)
         .send("You do not have permission to perform this action.");
     }
-
-    // Extract debt ID and new amount from request body
     const { id } = req.params;
     const { newAmount } = req.body;
-
-    // Find the debt by ID and update its amount
     await Debt.findByIdAndUpdate(id, { amount: newAmount });
-
-    // Redirect to the dashboard with a success message
     res.redirect("/dashboard");
   } catch (error) {
     console.error("Error updating debt:", error);
@@ -285,21 +243,14 @@ app.post("/update-debt/:id", isAuthenticated, async (req, res) => {
 
 app.post("/update-saving/:id", isAuthenticated, async (req, res) => {
   try {
-    // Ensure user is admin
     if (!req.user.isAdmin) {
       return res
         .status(403)
         .send("You do not have permission to perform this action.");
     }
-
-    // Extract saving ID and new amount from request body
     const { id } = req.params;
     const { newAmount } = req.body;
-
-    // Find the saving by ID and update its amount
     await Savings.findByIdAndUpdate(id, { amount: newAmount });
-
-    // Redirect to the dashboard with a success message
     res.redirect("/dashboard");
   } catch (error) {
     console.error("Error updating saving:", error);
@@ -307,20 +258,32 @@ app.post("/update-saving/:id", isAuthenticated, async (req, res) => {
   }
 });
 
-app.get("/savings", isAuthenticated, savingsController.getAllSavings);
-app.post("/savings", isAuthenticated, savingsController.createSaving);
-app.delete("/savings/:id", isAuthenticated, savingsController.deleteSaving);
-
-app.get("/debt", isAuthenticated, debtController.getAllDebt);
-app.post("/debt", isAuthenticated, debtController.createDebt);
-app.delete("/debt/:id", isAuthenticated, debtController.deleteDebt);
-
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send("Something went wrong!");
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send("Unauthorized Access!");
+  } else {
+    res.status(500).send("Something went wrong!");
+  }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const getTotalSavingsAmount = (savings) => {
+  let totalAmount = 0;
+  savings.forEach((saving) => {
+    totalAmount += saving.amount;
+  });
+  return totalAmount;
+};
+
+const getTotalDebtAmount = (debt) => {
+  let totalAmount = 0;
+  debt.forEach((debt) => {
+    totalAmount += debt.amount;
+  });
+  return totalAmount;
+};
